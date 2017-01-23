56f54b1b74fc3f250a449377e87e2dc00856f80d
nasheng <nasheng@uoko.com>
2017-01-23 11:35:59 +0800

    init and test branch

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    template: `<h1>Hello, {{name}}</h1>`
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+    name = 'This is a Angular2 greeting'
+}
